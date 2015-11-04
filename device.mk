2561d90d8b6256c85f670b082dfad038d8f31fc9
Raghavendra Prasad Nagaraj <prasr@codeaurora.org>
2015-11-04 12:54:18 +0530

    albus : services: Enable delay service restart
    This patch turns on system property "ro.am.reschedule_service".
    This helps to postpone services restarts during app launch.
    Only the services that are not relevant to current app launch are
    considered for postpone.
    
    Change-Id: I908be6dd7f7991b830a0440276fb9e2a0c0314c3

--- device.mk
+++ device.mk
@@ -278,6 +278,10 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
 	frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.am.reschedule_service=true
+
 # Power
 PRODUCT_PACKAGES += \
 	android.hardware.power@1.1-service-qti
