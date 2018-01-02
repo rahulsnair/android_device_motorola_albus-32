214967dcf4a8da945cbe5f87786d034b7e7610ca
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-02 16:05:51 -0500

    albus : Set IMS to use CodeAurora extension
    Change-Id: I64cba319986748e4dad206ca664ccda7eefb348a

--- device.mk
+++ device.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
 	$(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
+# IMS
+PRODUCT_PACKAGES += \
+	ims-ext-common
+
 # IRQ
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
