4224eaab8bdab4f0e6e35807a35a2aec8d128644
rahulsnair <rahulsnair30@gmail.com>
2018-08-04 22:01:36 +0000

    albus : build Gatekeeper HAL

--- device.mk
+++ device.mk
@@ -131,6 +131,11 @@ PRODUCT_PACKAGES += \
 	libfmjni \
 	init.qti.fm.sh
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+	android.hardware.gatekeeper@1.0-impl \
+	android.hardware.gatekeeper@1.0-service
+
 # GPS
 PRODUCT_PACKAGES += \
 	android.hardware.gnss@1.0-impl-qti \
