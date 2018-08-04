c48084c79ceec211912cb2347c4572c8fb655975
rahulsnair <rahulsnair30@gmail.com>
2018-08-04 21:55:20 +0000

    albus : build display packages

--- device.mk
+++ device.mk
@@ -97,6 +97,25 @@ PRODUCT_PACKAGES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Display
+PRODUCT_PACKAGES += \
+	android.hardware.graphics.allocator@2.0-impl \
+	android.hardware.graphics.allocator@2.0-service \
+	android.hardware.graphics.composer@2.1-impl \
+	android.hardware.graphics.composer@2.1-service \
+	android.hardware.graphics.mapper@2.0-impl \
+	android.hardware.memtrack@1.0-impl \
+	android.hardware.memtrack@1.0-service \
+	copybit.msm8953 \
+	gralloc.msm8953 \
+	hwcomposer.msm8953 \
+	libdisplayconfig \
+	liboverlay \
+	libqdMetaData.system \
+	libtinyxml \
+	memtrack.msm8953 \
+	libgenlock
+
 # GPS
 PRODUCT_PACKAGES += \
 	android.hardware.gnss@1.0-impl-qti \
