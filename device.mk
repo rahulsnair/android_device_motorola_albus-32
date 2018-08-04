ff69981c81e9897290b9664c8bee38d9581a24fb
rahulsnair <rahulsnair30@gmail.com>
2018-08-04 17:34:41 +0000

    albus : build bluetooth hidl

--- device.mk
+++ device.mk
@@ -43,6 +43,12 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
 	frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+	android.hardware.bluetooth@1.0-service \
+	android.hardware.bluetooth@1.0-service-qti \
+	android.hardware.bluetooth@1.0-impl
+
 # GPS
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
