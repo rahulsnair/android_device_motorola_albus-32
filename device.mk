0e32eb45bfd2815ca89a401bd996240de2b6ce00
rahulsnair <rahulsnair30@gmail.com>
2018-08-04 22:06:52 +0000

    albus : build media extension packages

--- device.mk
+++ device.mk
@@ -225,6 +225,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := \
 	framework-res
 
+# Media Extensions
+PRODUCT_PACKAGES += \
+	libavmediaserviceextensions \
+	libmediametrics \
+	libregistermsext \
+	mediametrics
+
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
