e75ec4e8d08914e991c2a1f93589cd09fcfc47ec
rahulsnair <rahulsnair30@gmail.com>
2018-08-05 15:55:04 +0000

    albus : build hidl base&manager
    Change-Id: I5f7f73576a0f32df3422f28823f06b1efaadf93d

--- device.mk
+++ device.mk
@@ -171,6 +171,11 @@ PRODUCT_PACKAGES += \
 	android.hardware.health@1.0-impl \
 	android.hardware.health@1.0-service
 
+# HIDL
+PRODUCT_PACKAGES += \
+	android.hidl.manager@1.0 \
+	android.hidl.base@1.0
+
 # IMS
 PRODUCT_PACKAGES += \
 	ims-ext-common
